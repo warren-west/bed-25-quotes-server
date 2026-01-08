a667e7faff760f0676cfa4e64772cf5664ab4b82
warren-west <teacher.warren.english@gmail.com>
2026-01-08 14:27:37 +0100

    Initial commit

---
+++ database.js
@@ -0,0 +1,54 @@
+const database = [
+    "It always seems impossible until it's done (Nelson Mandela)",
+    "Be the best version of you (Someone spiritual)",
+]
+
+// Get All Quotes from the Database:
+function getAllQuotes() {
+    // HTTP 200 OK
+    let result = ''
+    for (let item of database) {
+        result += item + '\n'
+    }
+
+    return { code: 200, data: result.trim(), message: "Retrieved all quotes from the database successfully!" }
+}
+
+// Get a quote from the database by ID:
+function getQuoteById(id) {
+    if (!database[id]) {
+        return { code: 404, data: "", message: `Quote with the ID: ${id} was not found.`}
+    }
+
+    return { code: 200, data: database[id], message: "Successfully retrieved a quote from the DB." }
+}
+
+// Add a new quote into the DB:
+function addQuote(quote) {
+    if (!quote) {
+        // 400 Bad Request
+        return { code: 400, message: "Invalid input." }
+    }
+
+    database.push(quote)
+    // HTTP 201 Created
+    return { code: 201, message: "Successfully added quote to the database", data: database[database.length - 1] }
+}
+
+// Delete a quote from the database, by ID:
+function deleteQuote() {
+
+}
+
+// Update the text of a quote in the db, by ID:
+function updateQuote() {
+
+}
+
+module.exports = {
+    getAllQuotes,
+    getQuoteById,
+    addQuote,
+    deleteQuote,
+    updateQuote,
+}
---
+++ server.js
@@ -0,0 +1,96 @@
+// Simple database of text quotes
+// The server should manage requests from clients, enabling them to:
+// 1. Get all quotes from the DB.
+// 2. Get a quotes from the DB, by ID.
+// 3. Add a new quote to the DB.
+// 4. Delete a quote from the DB.
+// 5. Update a quote text in the DB.
+
+// CRUD: Create, Read, Update, Delete
+// Quotes -> create, read, update and delete quotes
+
+// Steps:
+// 1. Import the http module.
+// 2. Configure the server.
+// 3. Run the server (listen).
+
+const http = require('http')
+const { getAllQuotes, getQuoteById, addQuote } = require('./database.js')
+
+http.createServer((req, res) => {
+    // Get METHOD and URL from request
+    const method = req.method
+    const url = req.url
+
+    console.log(`METHOD: ${method}`)
+    console.log(`URL: ${url}`)
+
+    // if method is GET (getAllQuotes or getOneQuoteById)
+        // if URL ends with /{:id} we want to get a single quote
+        // if it ends with /all we return all the quotes in the database
+    // if method is POST addQuote
+    // if method is PUT updateQuote
+    // if method is DELETE deleteQuote
+
+    if (method == "GET" && url.endsWith("/all")) {
+        // return all the quotes from the DB
+        const result = getAllQuotes()
+
+        console.log(result.message)
+
+        // construct a response:
+        res.statusCode = result.code
+        res.write(result.data)
+        res.end()
+        return
+
+    } else if (method == "GET") {
+        // return a single element by ID
+
+        // "/1".split('/') -> ["", "1"]
+        // "/22".split('/') -> ["", "22"]
+        const id = Number(url.split('/')[1])
+        const result = getQuoteById(id)
+        
+        console.log(result.message)
+        
+        // construct the response:
+        res.statusCode = result.code
+        res.write(result.data)
+        res.end()
+
+        return
+
+    } else if (method == "POST") {
+        // add a quote to the db
+
+        // get the text attached to the request body
+        let data = ""
+        req.on("data", (chunk) => {
+            data += chunk
+        })
+        req.on("end", () => {
+            console.log(data)
+
+            const result = addQuote(data)
+
+            console.log(result.message)
+
+            res.statusCode = result.code
+            res.write(result.data)
+            res.end()
+
+            return
+        })
+
+    } else if (method == "DELETE") {
+        // delete item from db
+
+    } else if (method == "PUT") {
+        // update item in DB
+
+    }
+})
+.listen(8080, () => {
+    console.log(`Server is running on port 8080...`)
+})
